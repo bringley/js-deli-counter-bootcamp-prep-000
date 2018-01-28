4023dcd2a0e3e7146f719d5152cd5b9457a06099
Brian Ringley <brian.ringley@wework.com>
2018-01-28 22:22:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,14 @@ function currentLine(line) {
     if (line.length === 0) {
     return 'The line is currently empty.'
     } else {
-      var msg = 'The line is currently: '
+      var msg = 'The line is currently:'
       for (let i = 0; i < line.length; i++) {
-        msg = msg + `${i + 1}. ${line[i]}, `
+        msg = msg + ` ${i + 1}. ${line[i]}`
+        if (i === line.length - 1) {
+          msg = msg
+        } else {
+          msg = msg + ','
+        }
       }
       return msg
     }
