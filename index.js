fdb3d9f2da923e07ad6236fba405fea2f2cd9082
Brian Ringley <brian.ringley@wework.com>
2018-01-28 22:14:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function nowServing(line) {
 
 function currentLine(line) {
     if (line.length === 0) {
-    return 'The line is currently empty'
+    return 'The line is currently empty.'
     } else {
       var msgStart = 'The line is currently: '
-      for (i = 0; i < line.length; i++) {
+      for (let i = 0; i < line.length; i++) {
         var msg = msgStart + `${i}. ${line[i]},`
       }
       return msg
